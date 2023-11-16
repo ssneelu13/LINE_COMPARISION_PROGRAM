38f85a75e8bd12c876744e28a4d58cb946c38a1e
Neelufer <ssneelu13@gmail.com>
2023-11-17 05:20:00 +0530

    welcome to line comparison program

---
+++ src/LINE_COMPARISON_USECASES/Line_Comparison_Program.java
@@ -0,0 +1,7 @@
+package LINE_COMPARISON_USECASES;
+
+public class Line_Comparison_Program {
+    public static void main(String[] args){
+        System.out.println("Welcome to line comparison program");
+    }
+}
